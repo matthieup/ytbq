078825f6eae6c0ecfb8989f7d8e2af556668968c
Matthieu PASINI <matthieu.pasini@gmail.com>
2026-04-15 16:24:16 +0700

    adding timestamps to logs

--- run.py
+++ run.py
@@ -2,4 +2,22 @@ import uvicorn
 from app.main import app
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=8000,
+        log_config={
+            "formatters": {
+                "default": {
+                    "()": "uvicorn.logging.DefaultFormatter",
+                    "fmt": "%(asctime)s - %(levelprefix)s %(message)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                },
+                "access": {
+                    "()": "uvicorn.logging.AccessFormatter",
+                    "fmt": "%(asctime)s - %(levelprefix)s %(client_addr)s - %(request_line)s %(status_code)s",
+                    "datefmt": "%Y-%m-%d %H:%M:%S",
+                },
+            },
+        },
+    )
